680d98f7f5e121234cbd3873409403dd6d1866a3
eFeN <131417689+eFeN23@users.noreply.github.com>
2026-05-28 08:10:30 -0500

    Agregando user_tools.py y registrando en environment

--- src/tau2/domains/enosa_masias/environment.py
+++ src/tau2/domains/enosa_masias/environment.py
@@ -2,6 +2,7 @@ from typing import Optional
 from tau2.data_model.tasks import Task
 from tau2.domains.enosa_masias.data_model import EnosaDB
 from tau2.domains.enosa_masias.tools import EnosaToolKit
+from tau2.domains.enosa_masias.user_tools import EnosaUserToolKit
 from tau2.domains.enosa_masias.utils import (
     ENOSA_DB_PATH, ENOSA_POLICY_PATH, ENOSA_TASK_SPLIT_PATH, ENOSA_TASK_SET_PATH,
 )
@@ -13,10 +14,17 @@ def get_environment(db: Optional[EnosaDB] = None, solo_mode: bool = False) -> En
     if db is None: db = EnosaDB.load(ENOSA_DB_PATH)
     
     tools = EnosaToolKit(db)
+    user_tools = EnosaUserToolKit(db)
+    
     with open(ENOSA_POLICY_PATH, "r", encoding="utf-8") as fp:
         policy = fp.read()
         
-    return Environment(domain_name="enosa_masias", policy=policy, tools=tools)
+    return Environment(
+        domain_name="enosa_masias", 
+        policy=policy, 
+        tools=tools, 
+        user_tools=user_tools
+    )
 
 def get_tasks(task_split_name: Optional[str] = "base") -> list[Task]:
     tasks = load_file(ENOSA_TASK_SET_PATH)
@@ -25,7 +33,6 @@ def get_tasks(task_split_name: Optional[str] = "base") -> list[Task]:
     if task_split_name is None: return tasks
     
     task_splits = get_tasks_split()
-    # Aquí validamos si el split (train, test, base) existe
     if task_split_name not in task_splits:
         raise ValueError(f"Split '{task_split_name}' no encontrado. Usa: {list(task_splits.keys())}")
         
